cdeec43c0fb2d9e0bcef55cda8bbccd2671a6369
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-01 17:35:53 +0100

    Adding input & validations

--- my-first-app/src/places/pages/NewPlace.js
+++ my-first-app/src/places/pages/NewPlace.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useCallback } from "react";
+
+import { VALIDATOR_REQUIRE } from "../../util/validators";
+import Input from "../../shared/components/FormElements/input";
+import './NewPlace.css';
+import { VALIDATOR_MINLENGTH } from "../../shared/util/validators";
 
 const NewPlace = () => {
-    return <h2>New Place works!</h2>
+
+    const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+    const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+    return <form className="place-form">
+        <Input 
+        id="title"
+        element="input" 
+        type="text" 
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}/>
+
+        <Input 
+        id="description"
+        element="textarea" 
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)"
+        onInput={descriptionInputHandler}/>
+    </form>
 };
 
 export default NewPlace;
