68339d5dbbccfe8126d0d705d98968435894fe91
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-07 17:51:25 +0100

    Add Express

--- Node/app.js
+++ Node/app.js
@@ -1,27 +1,22 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    console.log('INCOMING REQUEST');
-    console.log(req.method, req.url);
+const app = express();
 
-    if (req.method === 'POST'){
-        let body = '';
-        req.on('end', () => {
-            const userName = body.split('=')[1];
-            res.end('<h1>' + userName + '</h1>');
-        });
+app.use(bodyParser.urlencoded({ extended: false }));
 
-        req.on('data', (chunk) => {
-            body += chunk;
-        });
-    } else {
-
-        res.setHeader('Content-type', 'text/html');
-        res.end(
-            '<form method="POST"><input type="text" name="userName"/><button type="submit">Create User</button></form>'
-        );
-
-    }
+app.post('/user', (req, res, next) =>{
+    res.send('<h1>User: ' + req.body.username + '</h1>');
 });
 
-server.listen(5000)
+app.get('/', (req, res, next) => {
+    res.send(
+        '<form action="/user" method="POST"><input type="text" name="userName"/><button type="submit">Create User</button></form>'
+    );
+});
+
+        
+
+
+
+app.listen(5000)
