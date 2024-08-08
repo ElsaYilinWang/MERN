7b933f824df3381ecd57ae8782386a58199b1cc4
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-08 21:53:07 +0100

    bug fixed

--- Express101/nodeServer.js
+++ Express101/nodeServer.js
@@ -1,29 +1,49 @@
-// NODEJS IS the language
-// Express is node, a node module
+// http is native to NodeJS. We just have to ask for it
+const http = require('http');
 
-// http is a native module
-// const http = require('http');
-// express is a 3rd party module
-const express = require('express');
+// fs = file system module! fs is built into Node. see above
+// fs gives node access to THIS computers file system.
+const fs = require('fs');
 
-// An "app" is the express function (createAppliction inside the Express module)
-// invoked and is an Express appliction
-const app = express();
-
-// serve up static files! Only 1 line... take that nodejs
-app.use(express.static('public'));
-
-// all is a method, and it takes 2 args:
-// 1. route
-// 2. callback to run if the route is requested
-app.all('/', (req, res) => {
-    // Express handles the basic headers (status code, mime-type)! Awesome!
-    // read in Node.html
-    console.log(path.join(__dirname + '/node.html'))
-    res.sendFile(path.join(__dirname + '/node.html'))
-    // res.send(`<h1>This is the home page</h1>`)
-    // Express handles the end! Awesome!
+// the http module has a createServer method
+// takes 1 arg:
+// 1. callback, callback, has 2 args: req, res
+const server = http.createServer((req, res)=>{
+    console.log(`A request was made to: ${req.url}`)
+    if(req.url === '/'){
+        // the user wants the home page! we know, because the req object has / in the url property
+        // console.log(req)
+        // res = our way of responding to the requester
+        // http message 
+        // 1. start-line - CHECK
+        // 2. header
+        // 3. body
+        // writeHead takes 2 args:
+        // 1. status code
+        // 2. object for the mime-type
+        res.writeHead(200,{'content-type':'text/html'});
+        // res.write('');
+        const homePageHTML = fs.readFileSync('node.html')
+        res.write(homePageHTML)
+        res.end();
+    }else if(req.url === "/node.png"){
+        res.writeHead(200,{'content-type':'image/png'});
+        const image = fs.readFileSync('node.png')
+        res.write(image)
+        res.end();
+    }else if(req.url === "/styles.css"){
+        res.writeHead(200,{'content-type':'text/css'});
+        const css = fs.readFileSync('styles.css')
+        res.write(css)
+        res.end();        
+    }else{
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write(`<h4>Sorry, this isn't the page you're looking for!</h4>`);
+        res.end();        
+    }
 });
 
-app.listen(3000)
-console.log("The server is listening on port 3000...");
+// createServer returns an object with a listen method
+// listen takes 1 arg:
+// 1. port to listen for http traffic on
+server.listen(3000);
