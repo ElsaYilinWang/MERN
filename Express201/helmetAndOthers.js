8dc442075733d76fea763be67a3e7a2ac815fc3a
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-09 15:58:49 +0100

    express helmet

---
+++ Express201/helmetAndOthers.js
@@ -0,0 +1,23 @@
+// npm package called helmet
+// https://www.npmjs.com/package/helmet?activeTab=readme
+
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+
+app.use(helmet());
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// 1. static
+// 2. json
+// 3. urlencoded
+
+app.post('/ajax', (req, res)=>{
+    console.log(req.body);
+    res.json(["Test",1, 2, 3, 4])
+});
+
+app.listen(3000);
