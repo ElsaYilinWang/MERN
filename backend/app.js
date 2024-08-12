24b815cb68b393656bf9d6cb510f014f5219c7ba
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-12 22:06:35 +0100

    Users routes setup - Signup, Login, GetUsers

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const placeRoutes = require('./routes/place-routes');
-
+const placesRoutes = require('./routes/place-routes');
+const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('./api/places', placeRoutes); // => /api/places...
-
+app.use('./api/places', placesRoutes); // => /api/places...
+app.use('./api/users', usersRoutes);
 
 // Route to not-found document
 app.use((req, res, next) => {
