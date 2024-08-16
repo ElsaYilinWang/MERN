7c71d8c471f7e6dc87da628455eca6391df14a0e
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-08-16 16:33:57 +0100

    custom HTTP hook useHttpClient

--- my-first-app/src/user/pages/Users.js
+++ my-first-app/src/user/pages/Users.js
@@ -1,17 +1,50 @@
-import react from 'react';
+import React, { useEffect, useState } from 'react';
+
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-    const USERS=[
-        {id:'u1',
-        name:'Elsa Wang',
-        image:'https://en.wikipedia.org/wiki/Independent_Olympians_at_the_Olympic_Games',
-        places:3
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
         }
 
-    ];
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
 
-    return <UsersList items={USERS}/>;
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
